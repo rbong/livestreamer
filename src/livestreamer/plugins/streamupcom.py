da9caab202f5964d46af682ecadfe42a99dcf493
rbong <roger.bongers@yahoo.ca>
2016-04-16 12:34:13 +0000

    Fixed streamup plugin for API changes.

--- src/livestreamer/plugins/streamupcom.py
+++ src/livestreamer/plugins/streamupcom.py
@@ -5,8 +5,9 @@ from livestreamer.plugin import Plugin
 from livestreamer.plugin.api import http, validate
 from livestreamer.stream import RTMPStream, HLSStream
 
+_PLAYLIST_URL = "https://lancer.streamup.com/api/channels/{}s-channel/playlists"
+
 _url_re = re.compile("http(s)?://(\w+\.)?streamup.com/(?P<channel>[^/?]+)")
-_hls_manifest_re = re.compile('HlsManifestUrl:\\s*"//"\\s*\\+\\s*response\\s*\\+\\s*"(.+)"')
 
 class StreamupCom(Plugin):
     @classmethod
@@ -14,11 +15,11 @@ class StreamupCom(Plugin):
         return _url_re.match(url)
 
     def _get_streams(self):
-        res = http.get(self.url)
+        channel = _url_re.match(self.url).group('channel')
+        res = http.get(_PLAYLIST_URL.format(channel))
         if not res: return
-        match = _hls_manifest_re.search(res.text)
-        url = match.group(1)
-        hls_url = "http://video-cdn.streamup.com{}".format(url)
+        match = http.json(res)
+        hls_url = match[u'hls']
         return HLSStream.parse_variant_playlist(self.session, hls_url)
 
 __plugin__ = StreamupCom
